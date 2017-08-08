b35e2032827bb4aa8ca5dd04d27aeef35692a466
ranand027 <ranand027@gmail.com>
2017-08-07 21:31:51 -0700

    SampleProject2

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld
   public static void main(String args[])
 {
 
- System.out.println("Hai Hello World Testing GIT HUB New Version");
+ System.out.println("Hai Hello World Testing Changes New Version");
  
 }
 
